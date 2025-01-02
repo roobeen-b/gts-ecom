ad69c620d2ca4043ed5696c5d3d5964ecd117489
roobeen-b <rubinbaidhya@gmail.com>
2025-01-02 15:05:04 +0545

    Add margin top in search product list

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -24,11 +24,11 @@ const SearchPage = () => {
   }, [searchQuery]);
 
   return (
-    <div className="px-2 md:px-4 lg:px-6 py-4 grid grid-cols-1 md:grid-cols-[200px_1fr] min-h-screen">
+    <div className="px-2 md:px-4 lg:px-6 py-4 min-h-screen">
       <div className="flex flex-col px-2 md:px-4 lg:px-6">
         <h1 className="text-2xl font-bold">Search for Products</h1>
         <SearchBar />
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-4">
           {filteredProducts && filteredProducts.length > 0 ? (
             filteredProducts.map((product) => (
               <ProductCard key={product.productName} product={product} />
